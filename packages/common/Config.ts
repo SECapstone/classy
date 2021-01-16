f7b9a0e895a929425313ce0f35adcf540f683773
Braxton Hall <35436247+braxtonhall@users.noreply.github.com>
2021-01-15 18:14:55 -0800

    Plugin (#6)
    * Changes to support course-specific plugins for Classy.
    
    * Dockerfile accounts for plugin
    
    * Fix broken Dockerfile
    
    * Build the plugin in portal dockerfile
    
    * Remove extra lines from Dockerfile
    
    * Build plugin
    
    * Build plugin but only the backend
    
    * Try using relative plugin path
    
    * Restore Custom frontend views
    
    * Remove functionality of custom views
    
    * Update dockerfile
    
    * Use absolute path for plugin path
    
    * Update tsconfig
    
    * fix plugin paths
    
    * Update paths for plugin simplification
    
    * Don't bake .env into image
    
    * Restore .env import
    
    * Pass NAME to build
    
    * Clone the plugin
    
    * Allow branch of plugin to be specified
    
    * Fix syntax in Dockerfile
    
    * Remove reference to dir that doesn't exist
    
    * I have no idea why it is now fixed
    
    * Don't bake env into autotest image
    
    * Repair plugin paths
    
    Co-authored-by: Reid Holmes (MBA) <rtholmes@gmail.com>

--- packages/common/Config.ts
+++ packages/common/Config.ts
@@ -1,11 +1,9 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 import Log, {LogLevel} from "./Log";
-// const dotenv = require('dotenv');
 const result = dotenv.config({path: __dirname + '/../../.env'});
 
 if (result.error) {
-    Log.error("Failed to parse .env " + result.error);
-    throw result.error;
+    Log.warn("Failed to parse .env", result.error);
 }
 
 /**
@@ -26,6 +24,7 @@ export enum ConfigKey {
     org = "org",
     testorg = "testorg",
     testname = "testname",
+    plugin_fullpath = "plugin_fullpath",
 
     classlist_uri = "classlist_uri",
     classlist_username = "classlist_username",
@@ -102,6 +101,7 @@ export default class Config {
                 org:      process.env.ORG,
                 testorg:  process.env.ORGTEST,
                 testname: process.env.NAMETEST,
+                plugin_fullpath: process.env.PLUGIN_FULLPATH,
 
                 classlist_uri:      process.env.CLASSLIST_URI,
                 classlist_username: process.env.CLASSLIST_USERNAME,
--- packages/portal/frontend/src/app/custom/CustomAdminView.ts
+++ packages/portal/frontend/src/app/custom/CustomAdminView.ts
@@ -1,5 +1,17 @@
-import {AdminView} from "../views/AdminView";
+import Log from "../../../../../common/Log";
+import {AdminTabs, AdminView} from "../views/AdminView";
 
-export default class CustomAdminView extends AdminView {
+/**
+ * Stock Default Admin view
+ */
+export class CustomAdminView extends AdminView {
+    constructor(remoteUrl: string, tabs: AdminTabs) {
+        Log.info("CustomAdminView::<init>(..)");
+        super(remoteUrl, tabs);
+    }
 
+    public renderPage(name: string, opts: any) {
+        Log.info('CustomAdminView::renderPage( ' + name + ', ... ) - start; options: ' + JSON.stringify(opts));
+        super.renderPage(name, opts);
+    }
 }
